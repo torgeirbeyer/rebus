aba6f3783aa8f8c4434eafb81772dbd9b7c95135
Torgeir Beyer <torgeir.beyer@netlife.com>
2022-04-05 20:24:07 +0200

    initial commit

---
+++ schemas/post.js
@@ -0,0 +1,32 @@
+export default {
+  name: 'post',
+  title: 'Oppgave',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    },
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+    },
+  },
+}
